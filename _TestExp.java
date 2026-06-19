194ee38c44f81b654f54c6bb24f596625db81e7f
Angel Morales <artes23graficas@gmail.com>
2026-06-19 16:10:38 -0600

    Test de la tabla de verdad

---
+++ _TestExp.java
@@ -0,0 +1,6 @@
+﻿import java.util.Map;
+import tautoteacher2.core.logica.ExplicacionEducativaBuilder;
+public class T { public static void main(String[] a) {
+  var m = Map.of("estudiar","p","aprobar","q");
+  System.out.println(ExplicacionEducativaBuilder.construir("test", "(p → q)", "CONTINGENCIA", m).substring(0, 500));
+}}
